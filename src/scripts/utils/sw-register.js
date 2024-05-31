ed203ea0a277941ece6df0b07c0b8f3809d6ec68
alvin sidiq <123379611+alvinsidiq@users.noreply.github.com>
2024-06-01 00:17:59 +0700

    Update sw-register.js

--- src/scripts/utils/sw-register.js
+++ src/scripts/utils/sw-register.js
@@ -1,14 +1,21 @@
+import * as WorkboxWindow from 'workbox-window';
+
 const swRegister = async () => {
   if (!('serviceWorker' in navigator)) {
-    console.log('Service Worker not supported in the browser');
+    console.log('Service Worker is not supported in the browser');
     return;
   }
 
+  // Membuat instance Workbox dengan menggunakan path ke file service worker
+  const workbox = new WorkboxWindow.Workbox('/sw.bundle.js');
+
   try {
-    await navigator.serviceWorker.register('./sw.bundle.js');
-    console.log('Service worker registered');
+    // Mendaftarkan service worker
+    await workbox.register();
+    console.log('Service worker registered successfully');
   } catch (error) {
-    console.log('Failed to register service worker', error);
+    // Menangani error saat gagal mendaftarkan service worker
+    console.error('Failed to register service worker:', error);
   }
 };
 
